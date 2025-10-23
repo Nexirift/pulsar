75ee2fb1a7e5a3f40a7dc1c9777f95db86b81e09
Hazelnoot <acomputerdog@gmail.com>
2025-10-23 01:54:36 -0400

    make emoji tags reactive

--- packages/frontend/src/custom-emojis.ts
+++ packages/frontend/src/custom-emojis.ts
@@ -10,6 +10,15 @@ import { get, set } from '@/utility/idb-proxy.js';
 
 const storageCache = await get('emojis');
 export const customEmojis = shallowRef<Misskey.entities.EmojiSimple[]>(Array.isArray(storageCache) ? storageCache : []);
+export const customEmojiTags = computed(() => {
+	const tags = new Set<string>();
+	for (const emoji of customEmojis.value) {
+		for (const tag of emoji.aliases) {
+			tags.add(tag);
+		}
+	}
+	return Array.from(tags);
+});
 export const customEmojiCategories = computed<[ ...string[], null ]>(() => {
 	const categories = new Set<string>();
 	for (const emoji of customEmojis.value) {
@@ -60,17 +69,6 @@ export async function fetchCustomEmojis(force = false) {
 	set('lastEmojisFetchedAt', now);
 }
 
-let cachedTags;
 export function getCustomEmojiTags() {
-	if (cachedTags) return cachedTags;
-
-	const tags = new Set();
-	for (const emoji of customEmojis.value) {
-		for (const tag of emoji.aliases) {
-			tags.add(tag);
-		}
-	}
-	const res = Array.from(tags);
-	cachedTags = res;
-	return res;
+	return customEmojiTags.value;
 }
